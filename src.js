7b67c4386bd0a1f9f9bbc914487b5958f2ea3ce6
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-06 14:38:09 -0500

    Update src.js

--- src.js
+++ src.js
@@ -3,6 +3,7 @@ Xivid | JavaScript Kernel
 (c) speedyplane2247 2019   
 */
 // MAIN
+RAM.set("Starting Boot process!", 1)
 
 // RAM EMU
 var RAM = new Object();
@@ -12,7 +13,11 @@ RAM.set1[id] = value
 }
 RAM.get = function(id) {
 return RAM.set1[id]
-} 
+}
+// KERNEL ERRORS
+function kernelError(id) {
+alert("Kernel Shutdown @ "+id)
+}
 // LOAD EXTERNAL LIBS
 function includeJs(jsFilePath) {
     var js = document.createElement("script");
