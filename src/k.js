8dcdcdc3860dda8a2768acb9fe575df6775a79cb
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-15 17:22:26 -0400

    Update k.js

--- src/k.js
+++ src/k.js
@@ -1,2 +1,68 @@
 // XiViD 
 // Re-write!
+/*
+(c) speedyplane2247 2019
+*/
+/*
+  _      _     
+ | |    (_)    
+ | | __  _ ___ 
+ | |/ / | / __|
+ |   < _| \__ \
+ |_|\_(_) |___/
+       _/ |    
+      |__/     
+*/
+// Cryptography Functions
+// Failure Codes
+// Write to eRAM
+// Bool Sign
+// Codesign Bool Writer
+// RoV Authorization
+// eRAM Engine
+/*
+_       _     
+ | |     (_)    
+ | |__    _ ___ 
+ | '_ \  | / __|
+ | | | |_| \__ \
+ |_| |_(_) |___/
+        _/ |    
+       |__/     
+*/
+// Code-signing
+// Kernel Size Checker
+// Hardware Hashing
+// Unsigned Arena
+// Bool Sign
+// Read-Only Var
+// Read-Only Var Manager
+// Protected Vars
+// Protected Vars Getter
+/*
+_      _     
+ | |    (_)    
+ | |__   _ ___ 
+ | '_ \ | / __|
+ | |_) || \__ \
+ |_.__(_) |___/
+       _/ |    
+      |__/     
+      */
+// Read Hardware Fuses
+// Get VARS (Protected & Read-only)
+// Permit Codesign Bypass
+// Ask if CodeSign is required
+// Launch App
+/*
+        _     
+       (_)    
+  ___   _ ___ 
+ / __| | / __|
+ \__ \_| \__ \
+ |___(_) |___/
+      _/ |    
+     |__/ 
+*/
+// Signer UI
+// Program Launch
