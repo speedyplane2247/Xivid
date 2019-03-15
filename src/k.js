e133ab34cd6d2278f38daca577f7a8266aca0be4
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-15 17:25:38 -0400

    Update k.js

--- src/k.js
+++ src/k.js
@@ -4,6 +4,22 @@
 (c) speedyplane2247 2019
 */
 /*
+           _                        _   _ _ _                          
+           | |                      | | | (_) |                         
+   _____  _| |_ ___ _ __ _ __   __ _| | | |_| |__  _ __ __ _ _ __ _   _ 
+  / _ \ \/ / __/ _ \ '__| '_ \ / _` | | | | | '_ \| '__/ _` | '__| | | |
+ |  __/>  <| ||  __/ |  | | | | (_| | | | | | |_) | | | (_| | |  | |_| |
+  \___/_/\_\\__\___|_|  |_| |_|\__,_|_| |_|_|_.__/|_|  \__,_|_|   \__, |
+                                                                   __/ |
+                                                                  |___/ 
+*/
+function loadKext(filename) {
+var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+}
+
+/*
   _      _     
  | |    (_)    
  | | __  _ ___ 
